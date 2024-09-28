98a722081cfacda362ed7bf98d91e0fa56d997b8
Ahmed Elfouly <ahmedelfouly1999@gmail.com>
2024-09-28 15:49:59 +0300

    Modify temp_conversion_tool.py

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -7,7 +7,7 @@ def convert_to_celsius(fahrenheit):
 
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
-    return CELSIUS_TO_FAHRENHEIT_FACTOR * celsius + 32
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR + 32
 
 def main():
     temperature = int(input("Enter the temperature to convert: "))
