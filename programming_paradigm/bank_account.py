69bb3f2372ef836bc73b8b21b402d320ba283ed7
Ahmed Elfouly <ahmedelfouly1999@gmail.com>
2024-10-05 15:09:08 +0300

    Remove main-0.py and modify bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,10 +1,8 @@
 class BankAccount:
-    def __init__(self, account_balance=0) -> None:
+    def __init__(self, account_balance=0.0) -> None:
         self.account_balance = account_balance
-    
     def deposit(amount):
-        self.account_balance = self.account_balance + amount
-    
+        self.account_balance += amount
     def withdraw(amount):
         if self.account_balance > amount:
             self.account_balance = self.account_balance - amount
