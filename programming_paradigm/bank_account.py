c3695cb8f458bfce5e2f137090a4e3fcca5451c1
Ahmed Elfouly <ahmedelfouly1999@gmail.com>
2024-10-05 15:37:09 +0300

    Add main-0.py again and method arguments in bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,14 +1,14 @@
 class BankAccount:
     def __init__(self, account_balance=0.0) -> None:
         self.account_balance = account_balance
-    def deposit(amount):
+    def deposit(self, amount):
         self.account_balance += amount
-    def withdraw(amount):
+    def withdraw(self, amount):
         if self.account_balance > amount:
             self.account_balance = self.account_balance - amount
             return True
         else:
             return False
         
-    def display_balance():
+    def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
