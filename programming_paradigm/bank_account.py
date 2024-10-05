9d496b5047b79d8cf032b4043a595bc1175ac45d
Ahmed Elfouly <ahmedelfouly1999@gmail.com>
2024-10-05 15:03:52 +0300

    Add main-0.py and bank_account.py to programming_paradigm

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, account_balance=0) -> None:
+        self.account_balance = account_balance
+    
+    def deposit(amount):
+        self.account_balance = self.account_balance + amount
+    
+    def withdraw(amount):
+        if self.account_balance > amount:
+            self.account_balance = self.account_balance - amount
+            return True
+        else:
+            return False
+        
+    def display_balance():
+        print(f"Current Balance: ${self.account_balance}")
